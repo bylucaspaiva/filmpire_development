c45ac51c5363a3b42eef2ae70c837f6fc405ec03
Lucas Paiva <lucasp4iva@gmail.com>
2022-10-22 19:20:19 -0300

    feat: adding style to images and layout

--- src/components/Movie/styles.js
+++ src/components/Movie/styles.js
@@ -4,6 +4,26 @@ export default makeStyles((theme) => ({
   movie: {
     padding: '10',
   },
+  links: {
+    alignItems: 'center',
+    fontWeight: 'bolder',
+    [theme.breakpoints.up('xs')]: {
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    '&:hover': {
+      cursor: 'pointer',
+      text: 'none',
+    },
+  },
+  image: {
+    borderRadius: '20px',
+    height: '300px',
+    marginBottom: '10px',
+    '&:hover': {
+      transform: 'scale(1.05)',
+    },
+  },
   title: {
     color: theme.palette.text.primary,
     /// ellipsis: add dots on final of large titles
