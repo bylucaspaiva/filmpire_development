5949dea134286c6acf0d188da8207660fd21ad00
Lucas Paiva <lucasp4iva@gmail.com>
2022-10-21 23:38:39 -0300

    Feat: fetching data from api

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -1,8 +1,19 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const tmdbApiKey = process.env.TMDB_KEY;
+const tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
 
+// https://api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=en-US
+// https://api.themoviedb.org/3/movie/550?api_key=172436804d4091688aa4082df93414c9
+const page = 1;
 export const tmdbApi = createApi({
   reducerPath: 'tmdbApi',
-  baseQuery: fetchBaseQuery({ baseUrl: '' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3' }),
+  endpoints: (builder) => ({
+    //* Get Movies By Type
+    getMovies: builder.query({
+      query: () => `movie/popular?page=${page}&api_key=${tmdbApiKey}`,
+    }),
+  }),
 });
+
+export const { useGetMoviesQuery } = tmdbApi;
