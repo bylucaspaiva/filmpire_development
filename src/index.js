4593d74cdcb7b7c3bdef81a96bd095e7daf87bc8
Lucas Paiva <lucasp4iva@gmail.com>
2022-10-21 22:27:21 -0300

    feat: adding api

--- src/index.js
+++ src/index.js
@@ -3,16 +3,20 @@ import ReactDOM from 'react-dom';
 // eslint-disable-next-line import/no-unresolved
 import { BrowserRouter } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Provider } from 'react-redux';
 
 import App from './components/App';
+import store from './app/store';
 
 const theme = createTheme({});
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ThemeProvider>,
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </Provider>,
   document.getElementById('root'),
 );
