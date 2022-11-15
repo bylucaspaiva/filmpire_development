e7b63899bc8adf6b98655be38cad927867bf6bb9
Lucas Paiva <lucasp4iva@gmail.com>
2022-11-15 17:54:28 -0300

    Feat: add sidebar images

---
+++ src/assets/genres/index.js
@@ -0,0 +1,10 @@
+import action from './action.png';
+import adventure from './adventure.png';
+import animation from './animation.png';
+import comedy from './comedy.png';
+import crime from './crime.png';
+import documentary from './documentary.png';
+import drama from './drama.png';
+import family from './family.png';
+import fantasy from './fantasy.png';
+
