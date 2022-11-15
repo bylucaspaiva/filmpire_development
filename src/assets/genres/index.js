337a61e864688c81dd3a1809562ed48565d970e3
Lucas Paiva <lucasp4iva@gmail.com>
2022-11-15 18:07:20 -0300

    feat add image index

--- src/assets/genres/index.js
+++ src/assets/genres/index.js
@@ -7,4 +7,43 @@ import documentary from './documentary.png';
 import drama from './drama.png';
 import family from './family.png';
 import fantasy from './fantasy.png';
+import scienceFiction from './fiction.png';
+import history from './history.png';
+import horror from './horror.png';
+import mistery from './mistery.png';
+import music from './music.png';
+import romance from './romance.png';
+import thriller from './thriller.png';
+import tvMovie from './tv.png';
+import war from './war.png';
+import western from './western.png';
+
+import popular from './Popular.png';
+import upcoming from './Upcoming.png';
+import topRated from './Top.png';
+
+export default {
+  action,
+  adventure,
+  animation,
+  comedy,
+  crime,
+  documentary,
+  drama,
+  family,
+  fantasy,
+  'science fiction': scienceFiction,
+  history,
+  horror,
+  mistery,
+  music,
+  romance,
+  thriller,
+  'tv movie': tvMovie,
+  war,
+  western,
+  popular,
+  upcoming,
+  'top rated': topRated,
+};
 
