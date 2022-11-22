3a997869655ce9f1f5afa725c2c3720597047cd3
Lucas Paiva <lucasp4iva@gmail.com>
2022-11-21 23:00:26 -0300

    Retira console.log

--- src/components/Actors/Actors.jsx
+++ src/components/Actors/Actors.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 
-const Actors = () => {
-  console.log('Actors');
-  return (
-    <div>
-      <h1>Actors</h1>
-    </div>
-  );
-};
+const Actors = () => (
+  <div>
+    <h1>Actors</h1>
+  </div>
+);
 
 export default Actors;
