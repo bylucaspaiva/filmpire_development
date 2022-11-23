b7c772c941c9ace28e858650b0f6d01baa5ae397
Lucas Paiva <lucasp4iva@gmail.com>
2022-11-23 20:27:26 -0300

    Feat: pull info from redux slice for profile page

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { userSelector, setUser } from '../../features/auth';
 
-const Profile = () => (
-  <div>
-    <h1>Profile</h1>
-  </div>
-);
+const Profile = () => {
+  //* This variable must have the same name it has in store.js
+  const { user } = useSelector(userSelector);
+  console.log(user.username);
+
+  return (
+    <div>
+      <h1>Profile - {user.username}</h1>
+    </div>
+  );
+};
 
 export default Profile;
