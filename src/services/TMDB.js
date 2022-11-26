7f1cffad7bf2570085ab14528647e06881eea836
Lucas Paiva <lucasp4iva@gmail.com>
2022-11-25 23:37:38 -0300

    feat: add redux toolkit for recomendation movies

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -35,6 +35,10 @@ export const tmdbApi = createApi({
     getMovie: builder.query({
       query: (id) => `/movie/${id}?append_to_response=videos,credits&api_key=${tmdbApiKey}`,
     }),
+    //* Get user specific lists
+    getRecomendations: builder.query({
+      query: (movie_id, list) => `/movie/${movie_id}/${list}?api_key=${tmdmApiKey}`,
+    }),
   }),
 });
 
@@ -42,4 +46,5 @@ export const {
   useGetMoviesQuery,
   useGetGenresQuery,
   useGetMovieQuery,
+  useGetRecomendationsQuery,
 } = tmdbApi;
