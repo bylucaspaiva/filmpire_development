67ef10f7d590cfb07456c1149d9038350e5b936e
Lucas Paiva <lucasp4iva@gmail.com>
2022-11-27 23:59:12 -0300

    fix number of movies on small devices

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -10,7 +10,9 @@ const Movies = () => {
   const [page, setPage] = useState(1);
   const { genreIdOrCategoryName, searchQuery } = useSelector((state) => state.currentGenreOrCategory);
   const { data, error, isFetching } = useGetMoviesQuery({ genreIdOrCategoryName, page, searchQuery });
+  const lg = useMediaQuery((theme) => theme.breakpoints.only('lg'));
 
+  const numberOfMovies = lg ? 22 : 24;
   if (isFetching) {
     return (
       <Box display="flex" justifyContent="center">
@@ -34,7 +36,7 @@ const Movies = () => {
   if (error) return 'An error as ocurred.';
   return (
     <div>
-      <MovieList movies={data} numberOfMovis={24} />
+      <MovieList movies={data} numberOfMovis={numberOfMovies} />
       <Pagination currentPage={page} setPage={setPage} totalPages={data.total_pages} />
     </div>
   );
